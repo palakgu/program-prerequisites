6ffe975f18a5353b80746835037a7fd5688c565f
palak gupta <palakofficial21@gmail.com>
2025-01-08 11:07:28 +0530

    task4

---
+++ Task4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ class Task4{
+public static void main(String[] args){
+System.out.println("enter the radious of the circle");
+Scanner sc = new Scanner(System.in);
+double r = sc.nextDouble();
+double Area ;
+Area = 3.14*r*r;
+System.out.println("Area of the circle"+ Area);
+
+
+
+}
+
+
+
+}
