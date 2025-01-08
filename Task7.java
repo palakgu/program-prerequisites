cb49e520cd1ce3b362006bcd9584b1fee8fe4403
palak gupta <palakofficial21@gmail.com>
2025-01-08 11:30:31 +0530

    task 7 completed

---
+++ Task7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Task7{
+public static void main(String[] args){
+System.out.print("enter the length and width of the rectangle");
+
+Scanner sc = new Scanner(System.in);
+float length = sc.nextFloat();
+float width = sc.nextFloat();
+float Perimeter;
+Perimeter = 2 * (length + width);
+System.out.println(Perimeter);
+
+} 
+
+
+}
