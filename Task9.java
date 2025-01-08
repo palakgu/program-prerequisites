37931ac41a7b0ccebb21d29d7d9263de12c72cb0
palak gupta <palakofficial21@gmail.com>
2025-01-08 11:45:07 +0530

    task 9 completed

---
+++ Task9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Task9{
+public static void main(String[] args){
+System.out.println("Enter the there numbers");
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c = sc.nextInt();
+ float Average = (a+b+c)/3;
+System.out.println(Average);
+
+
+}
+
+
+}
